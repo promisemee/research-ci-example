cadbabad48e70e364281114c4925ea2a2a8dc251
Kim Dain <promise_mee@daum.net>
2021-09-11 21:50:32 +0900

    modify main_test

--- main_test.py
+++ main_test.py
@@ -5,8 +5,8 @@ import main
 
 class MainTest(unittest.TestCase):
     def test_helloworld(self):
-        ret = main.helloworld("test")
-        self.assertEqual(ret, "Hello World! Chris!")
+        ret = main.helloworld("Test")
+        self.assertEqual(ret, "Hello World! Test!")
 
 
 if __name__ == "__main__":
